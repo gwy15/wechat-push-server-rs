1c2de445142a28e93941245ff9e39474bd8abafd
gwy15 <gwy15thu@gmail.com>
2020-04-08 19:41:54 +0800

    Change logging library to log4rs

--- src/logging.rs
+++ src/logging.rs
@@ -1,22 +1,4 @@
-use chrono::Local;
-use env_logger::Env;
-use log::info;
-use std::io::Write;
-
 pub fn init_logger() {
-    let env = Env::default().default_filter_or("info");
-    env_logger::Builder::from_env(env)
-        .format(|buf, record| {
-            writeln!(
-                buf,
-                "({t}) {level} [{module}:{line}] {args}",
-                t = Local::now().format("%Y-%m-%d %H:%M:%S"),
-                level = buf.default_styled_level(record.level()),
-                module = record.module_path().unwrap_or("<unknown>"),
-                line = record.line().unwrap_or(0),
-                args = &record.args()
-            )
-        })
-        .init();
-    info!("env logger initialized");
+    log4rs::init_file("config/log4rs.yml", Default::default()).unwrap();
+    log::info!("logging initialized");
 }
